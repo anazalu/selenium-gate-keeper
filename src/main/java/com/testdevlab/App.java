f76ccfebe23308a263e15b6b4dc3dce4d938d441
anastasijazalunina <anastasijazalunina@anastasijazaluninas-MacBook-Pro.local>
2024-03-25 14:49:49 +0200

    add initial files

---
+++ src/main/java/com/testdevlab/App.java
@@ -0,0 +1,9 @@
+package com.testdevlab;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/testdevlab/MyTestClass.java
@@ -0,0 +1,40 @@
+package com.testdevlab;
+
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+//import org.openqa.selenium.support.ui.ExpectedConditions;
+//import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class MyTestClass {
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("https://edruva.lv/sports/");
+    }
+
+    @Test
+    public void testTitle() {
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Sports - eDruva.lv";
+        Assert.assertEquals(actualTitle, expectedTitle, "Title mismatch");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }    
+}
